77282d8580e893cd01be362a79e4687eb8b6c8fd
Prachikaag <prachi11j@gmail.com>
2024-04-10 18:02:04 -0400

    Initial Commit

---
+++ Instagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by PRACHIKA AGARWAL on 10/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by PRACHIKA AGARWAL on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
