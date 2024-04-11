cb2b2678cb1d79eea0b9d808ac3d38e1596c2bc4
Prachikaag <prachi11j@gmail.com>
2024-04-10 20:29:31 -0400

    building firebase connection and view model for content view

---
+++ Instagram/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  Instagram
+//
+//  Created by PRACHIKA AGARWAL on 10/04/24.
+//
+
+import Foundation
---
+++ Instagram/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  Instagram
+//
+//  Created by PRACHIKA AGARWAL on 10/04/24.
+//
+
+import Foundation
