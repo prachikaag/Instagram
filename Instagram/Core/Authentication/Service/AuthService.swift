f34e4896c845e2b0076546f7ffdb3faf3f354ae6
Prachikaag <prachi11j@gmail.com>
2024-04-10 22:58:59 -0400

    Setup data to start populating on Firestore database

--- Instagram/Core/Authentication/Service/AuthService.swift
+++ Instagram/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import FirebaseFirestore
+import Firebase
 //contains functions authenticating user
 
 class AuthService {
@@ -18,7 +21,7 @@ class AuthService {
     init(){
         self.userSession = Auth.auth().currentUser
     }
-    
+    @MainActor
     func login(withEmail email: String, password: String) async throws{
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -27,15 +30,16 @@ class AuthService {
             print("DEBUG: LOGIN FAILED \(error.localizedDescription)")
         }
     }
-    
+    @MainActor
     func createUser(email: String, password: String, username: String) async throws{
         do{
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
+            print("DEBUG: Uploaded user data")
         } catch {
             print("DEBUG: SIGNUP FAILED \(error.localizedDescription)")
         }
-//        print("email is \(email)")
     }
     
     func loadUserData() async throws{
@@ -45,5 +49,11 @@ class AuthService {
     func signout(){
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else {return}
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 }
